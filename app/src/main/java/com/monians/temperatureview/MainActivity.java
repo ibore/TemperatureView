146d267e551bd9bcccb388bff06d92bffd562af4
Ibore <bore521@live.com>
2017-02-24 17:04:59 +0800

    已完成

--- app/src/main/java/com/monians/temperatureview/MainActivity.java
+++ app/src/main/java/com/monians/temperatureview/MainActivity.java
@@ -1,13 +1,38 @@
 package com.monians.temperatureview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private TemperatureView temperatureView;
+    private SeekBar seekBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        temperatureView = (TemperatureView) findViewById(R.id.temperatureView);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        temperatureView.start(80);
+        seekBar.setProgress(80 + 40);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Toast.makeText(getApplicationContext(), progress - 40 + "", Toast.LENGTH_SHORT).show();
+                temperatureView.start(progress - 40);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
